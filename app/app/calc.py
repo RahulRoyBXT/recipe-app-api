388266449b963aaf8f267e57fe6c4288360cca20
Rahul Roy <rahulroy.it2021@nsec.ac.in>
2024-01-21 14:35:49 +0530

    fix lint

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,11 @@
 """
 Calculator
-
 """
 
-def add(x,y):
-    """Add x and y and return ."""
+def add(x, y):
+    """Add x and y and return."""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x from y and return."""
     return y - x
