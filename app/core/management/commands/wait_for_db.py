0fef67d837bb1ff504f2b25f16610821197db8a9
Rahul Roy <rahulroy.it2021@nsec.ac.in>
2024-01-21 17:16:01 +0530

    mock

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""
+Dango command to wait for the database to be available.
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Daango command to wait for database"""
+
+    def handle(self, *args, **options):
+        pass
